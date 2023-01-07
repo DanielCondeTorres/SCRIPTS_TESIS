00065a9fa5cae659cd1beedc41334014c1330452
Daniel Conde Torres <117435891+DanielCondeTorres@users.noreply.github.com>
2023-01-07 16:23:58 +0100

    Update obtener_residuos.py

--- SCRIPTS_RANDOM/obtener_residuos.py
+++ SCRIPTS_RANDOM/obtener_residuos.py
@@ -1,39 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
-Numero_beads=12
-numero_beads=list(range(1,13))
-print('p',numero_beads)
-for i in numero_beads:
-    file_in = open('aa_ref_1b3e_autopsf.pdb')
+import optparse
+
+def main() :
+    parser = optparse.OptionParser(version='%prog version 1.0')
+    parser.add_option('-f', '--infile', help='input pdb file)', action='store')
+    options, arguments = parser.parse_args()
+#****************************************************************************
+    file_in = open(str(options.infile),'r')
     bead=[]
     pos=[]
     neg=[]
     neutro=[]
     apolar=[]
     aromatico=[]
-    ########################
-
-    next(file_in)
+    #next(file_in)
     for line in file_in:
-        if 'CA' in line:
-            if float(line.split()[10])==i:
-                bead.append(str(line.split()[3]))
-                if str(line.split()[3])==('GLY' or 'MET' or 'ALA' or 'PRO' or 'VAL' or 'ILE' or 'LEU'):
-                    apolar.append(str(line.split()[3]))
-                elif str(line.split()[3])==('PHE' or 'TRP'):
-                    aromatico.append(str(line.split()[3]))
-                elif str(line.split()[3])==('SER' or 'THR' or 'TYR' or 'CYS' or 'GLN' or 'ASN'):
-                    neutro.append(str(line.split()[3]))
-                elif str(line.split()[3])==('ASP' or 'GLU'):
-                    neg.append(str(line.split()[3]))
-                elif str(line.split()[3])==('LYS' or 'ARG' or 'HIS'):
-                    pos.append(str(line.split()[3]))
+        if 'CA' in line or 'BB' in line:
+            bead.append(str(line.split()[3]))
+            if str(line.split()[3])in ['GLY','MET','ALA','PRO','VAL','ILE','LEU']:
+                apolar.append(str(line.split()[3]))
+            elif str(line.split()[3]) in ['PHE','TRP']:
+                aromatico.append(str(line.split()[3]))
+            elif str(line.split()[3])in ['SER','THR','TYR','CYS','GLN','ASN']:
+                neutro.append(str(line.split()[3]))
+            elif str(line.split()[3]) in ['ASP','GLU']:
+                neg.append(str(line.split()[3]))
+            elif str(line.split()[3]) in ['LYS','ARG','HIS']:
+                pos.append(str(line.split()[3]))
+        if 'ENDMDL' in line:
+            break
     file_in.close()
-    print('BEAD:',i)
+    print('BEAD:',len(bead))
     print('RESIDUOS',bead)
     print('RESIDUOS POLARES:',len(pos)+len(neg)+len(neutro))
     print('RESIDUOS APOLARES: ',len(apolar))
     print('RESIDUOS AROMATICOS: ',len(aromatico))
     print('NEGATIVOS: ',len(neg))
     print('POSITIVOS: ',len(pos))
+    print('Polar Neutro: ',len(neutro))
     print('#######################################')
+if __name__=="__main__" :
+    main()
+~                   
