f4ba365c5b59379192eb08b90883600ccfc75114
Daniel Conde Torres <117435891+DanielCondeTorres@users.noreply.github.com>
2023-01-11 14:28:22 +0100

    Create eliminar_lineas_malas_hills.py

---
+++ MODIFICAR_HILLS/eliminar_lineas_malas_hills.py
@@ -0,0 +1,11 @@
+#Si el HILLS tiene alguna linea mal
+#new = open('HILLS_CORREGIDO2','w+')
+#with open( 'HILLS', 'r+' ) as file:
+#    for line in file:
+#        if len(line.split()) == 7:
+#            try:
+#                if not round(float(line.split()[0]))==5 :#or float(line.split()[1])<0:#ver primer valor de hills, si es negativo así
+#                    new.write(line)
+#            except ValueError:
+#                pass
+#new.close()
