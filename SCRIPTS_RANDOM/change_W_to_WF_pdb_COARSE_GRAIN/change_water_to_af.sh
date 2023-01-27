b78f0189f1649f7b77883afcd0da1ed16053d253
Daniel Conde Torres <117435891+DanielCondeTorres@users.noreply.github.com>
2023-01-27 22:59:26 +0100

    Update change_water_to_af.sh

--- SCRIPTS_RANDOM/change_W_to_WF_pdb_COARSE_GRAIN/change_water_to_af.sh
+++ SCRIPTS_RANDOM/change_W_to_WF_pdb_COARSE_GRAIN/change_water_to_af.sh
@@ -1,10 +1,12 @@
 #/bin/bash
-python change_W_to_AF_percentage.py -f equilibrado.pdb #name of the .pdb
+python change_W_to_AF_percentage.py -f equilibrado.pdb
 
 grep -wv "WF" new.pdb > new2.pdb
+grep -wv 'TER' new2.pdb > new22.pdb
+grep -wv 'ENDMDL' new22.pdb > new222.pdb
 grep -w "WF" new.pdb > new3.pdb
 
 grep -w "TER" new.pdb > new4.pdb
 grep -w "ENDMDL" new.pdb > new5.pdb
-cat new2.pdb new3.pdb new4.pdb new5.pdb > output.pdb
+cat new222.pdb new3.pdb new4.pdb new5.pdb > output.pdb
 rm new.pdb new?.pdb
