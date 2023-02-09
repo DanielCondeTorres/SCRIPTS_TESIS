9626cb5a3f680d8a851afaa9a4f3c353a2773462
Daniel Conde Torres <117435891+DanielCondeTorres@users.noreply.github.com>
2023-02-09 10:30:53 +0100

    Update eliminar_lineas_malas_hills.py

--- MODIFICAR_HILLS/eliminar_lineas_malas_hills.py
+++ MODIFICAR_HILLS/eliminar_lineas_malas_hills.py
@@ -1,11 +1,12 @@
 #Si el HILLS tiene alguna linea mal
-#new = open('HILLS_CORREGIDO2','w+')
-#with open( 'HILLS', 'r+' ) as file:
-#    for line in file:
-#        if len(line.split()) == 7:
-#            try:
-#                if not round(float(line.split()[0]))==5 :#or float(line.split()[1])<0:#ver primer valor de hills, si es negativo así
-#                    new.write(line)
-#            except ValueError:
-#                pass
+new = open('HILLS_CORREGIDO2','w+')
+with open( 'HILLS', 'r+' ) as file:
+    for line in file:
+        if len(line.split()) == 7:#numero lineas del HILLS
+            try:
+                # problema de ALEX
+                if not round(float(line.split()[0]))==5 :#or float(line.split()[1])<0:#ver primer valor de hills, si es negativo así
+                    new.write(line)
+            except ValueError:
+                pass
 #new.close()
